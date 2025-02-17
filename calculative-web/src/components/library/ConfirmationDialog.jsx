21d095cdc6a290143cae60fff5bff2f597de67b6
= <=>
2025-02-17 21:29:31 +0800

    Fix javascript error

--- calculative-web/src/components/library/ConfirmationDialog.jsx
+++ calculative-web/src/components/library/ConfirmationDialog.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
-import { PrimaryButton, SecondaryButton,DestructivePrimaryButton } from '../../styles/buttonStyles'; // Import custom buttons
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
 
 const ConfirmationDialog = ({
   open,
@@ -11,63 +10,26 @@ const ConfirmationDialog = ({
   secondaryButtonLabel,
   onPrimaryButtonClick,
   onSecondaryButtonClick,
-  countdown = false, // New parameter to control countdown
-  countdownTime = 5, // Default countdown time in seconds
-  isPrimaryButtonNegative = false, 
+  countdown,
+  countdownTime,
+  isPrimaryButtonNegative,
 }) => {
-  const [timeLeft, setTimeLeft] = useState(countdownTime);
-
-  useEffect(() => {
-    if (countdown && open) {
-      setTimeLeft(countdownTime);
-      const timer = setInterval(() => {
-        setTimeLeft((prevTime) => {
-          if (prevTime === 1) {
-            clearInterval(timer);
-            onPrimaryButtonClick(); // Automatically click the primary button
-            return 0;
-          }
-          return prevTime - 1;
-        });
-      }, 1000);
-      return () => clearInterval(timer);
-    }
-  }, [countdown, open, countdownTime, onPrimaryButtonClick]);
-
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>
-        <DialogContentText>
-        {message}
-          {countdown && (
-            <div>
-              <span>{primaryButtonLabel} in </span>
-              <span style={{ 
-                color: isPrimaryButtonNegative ? 'red' : 'blue', 
-                textDecoration: 'underline', 
-                fontSize: '1.2em' // Increase font size
-              }}>
-                {timeLeft}
-              </span>
-              <span> seconds...</span>
-            </div>
-          )}
-        </DialogContentText>
+        <p>{message}</p>
       </DialogContent>
       <DialogActions>
-        <SecondaryButton onClick={onSecondaryButtonClick}>
+        <Button onClick={onSecondaryButtonClick} color="primary">
           {secondaryButtonLabel}
-        </SecondaryButton>
-        {isPrimaryButtonNegative ? (
-          <DestructivePrimaryButton onClick={onPrimaryButtonClick}>
-            {primaryButtonLabel}
-          </DestructivePrimaryButton>
-        ) : (
-          <PrimaryButton onClick={onPrimaryButtonClick}>
-            {primaryButtonLabel}
-          </PrimaryButton>
-        )}
+        </Button>
+        <Button
+          onClick={onPrimaryButtonClick}
+          color={isPrimaryButtonNegative ? 'secondary' : 'primary'}
+        >
+          {primaryButtonLabel}
+        </Button>
       </DialogActions>
     </Dialog>
   );
