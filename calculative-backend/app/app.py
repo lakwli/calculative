be22b58bbdc349e3dde8af36114189e4e42ec639
lakwli <kwailing.freedom@gmail.com>
2025-03-04 07:36:01 +0000

    refactor

--- calculative-backend/app/app.py
+++ calculative-backend/app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from datetime import datetime
-from fin.run_yahoo import StockCal
+from app.fin.stock_cal import StockCal
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
